58d6a8c27d780f55f0e8daa21bbb5c917c1aa127
Alan Park <28246519+alansdpark@users.noreply.github.com>
2022-09-24 16:10:51 -0700

    Initial commit

---
+++ CreateStatements.sql
@@ -0,0 +1,8 @@
+-- Create Customer Table (Not done)
+CREATE TABLE Customer (
+    Cust_ID int NOT NULL AUTO_INCREMENT,
+    FirstName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
+    HomeAddress varchar(255) NOT NULL,
+    PhoneNumber varchar(255) NOT NULL
+);
---
+++ README.md
@@ -0,0 +1,2 @@
+# CMPE 172 Group Project
+All relevant SQL statements in this repository.
