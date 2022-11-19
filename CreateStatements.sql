5ddd74271c5cd8cc1b0cd83aaa509761eeb8fbf9
alenba2 <baekallen@gmail.com>
2022-11-18 16:45:59 -0800

    Moved all data from TicketType.sql to CreateStatements.sql && made InsertData.sql file

--- CreateStatements.sql
+++ CreateStatements.sql
@@ -34,3 +34,17 @@ CREATE TABLE Airport (
     Airport_name varchar(255) NOT NULL,
     Airport_ID char(9)  NOT NULL
 );
+
+create table TicketType(
+Type_ID char(9) NOT NULL,
+Price Decimal(10,2),
+Mem_ID char(9),
+PRIMARY KEY(Type_ID)
+);
+
+create table Membership(
+Category varchar(9) Not Null,
+Mem_ID char(9) Not Null,
+Mem_Name varchar(9) NOT NULL,
+PRIMARY KEY(Mem_ID)
+);
---
+++ InsertData.sql
@@ -0,0 +1,28 @@
+
+-- Customer
+insert into Customer values (11111,"John","Doe","123 Fairfont Ave",14029393333);
+insert into Customer values (11112,"Allen","Baek","111 June Crop Dr",12222222222);
+
+-- Airplane
+
+insert into Airplane values (300,1000,123456,"A320");
+
+-- Airplane_Schedule
+
+insert into Airplane_Schedule values (DATE '2022-11-10','2022-11-12',12345,1,2);
+
+-- Ticket
+
+insert into Ticket values (11111, 12345,"John Doe",'2022-11-10','2023-11-10');
+
+-- Airport
+
+insert into Airport values ("San Francisco Airport",1);
+
+-- TicketType
+
+insert into TicketType values (12345, 200.20, 11111);
+
+-- Membership
+
+insert into Membership values ("GOLD", 11111, "JOSE's GOLD MEMBERSHIP");
