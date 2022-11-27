8c7617caed1aef3b6e77aff0583032a266f9c8e3
Allen Baek <36782804+alenba2@users.noreply.github.com>
2022-11-26 17:05:26 -0800

    Update InsertData.sql

--- InsertData.sql
+++ InsertData.sql
@@ -5,7 +5,11 @@ insert into Customer values (11112,"Allen","Baek","111 June Crop Dr",12222222222
 
 -- Airplane
 
-insert into Airplane values (300,1000,123456,"A320");
+insert into Airplane values ("A3",1000,123456,"A320");
+insert into Airplane values ("23",1500,111111,"787");
+insert into Airplane values ("B2",300,122222,"767");
+insert into Airplane values ("C7",100,111444,"A330");
+insert into Airplane values ("D1",500,111112,"747");
 
 -- Airplane_Schedule
 
@@ -18,11 +22,19 @@ insert into Ticket values (11111, 12345,"John Doe",'2022-11-10','2023-11-10');
 -- Airport
 
 insert into Airport values ("San Francisco Airport",1);
+insert into Airport values ("Denver International Airport",2);
+insert into Airport values ("John F. Kennedy International Airport",3);
+insert into Airport values ("Antalya Airport",4);
+insert into Airport values ("Singapore Changi Airport",5);
 
 -- TicketType
 
 insert into TicketType values (12345, 200.20, 11111);
+insert into TicketType values (12222, 220.69, 11112);
+insert into TicketType values (12333, 250.89, 11113);
 
 -- Membership
 
 insert into Membership values ("GOLD", 11111, "JOSE's GOLD MEMBERSHIP");
+insert into Membership values ("SILVER", 11112, "Master Card MEMBERSHIP");
+insert into Membership values ("BRONZE", 11113, "Well's Fargo MEMBERSHIP");
