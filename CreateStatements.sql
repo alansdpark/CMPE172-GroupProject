b1f9bafde1b01d82f2d2cea77dbfafe98a39a180
Allen Baek <36782804+alenba2@users.noreply.github.com>
2022-11-28 16:30:00 -0800

    Update CreateStatements.sql

--- CreateStatements.sql
+++ CreateStatements.sql
@@ -4,14 +4,16 @@ CREATE TABLE Customer (
     FirstName varchar(255) NOT NULL,
     LastName varchar(255) NOT NULL,
     HomeAddress varchar(255) NOT NULL,
-    PhoneNumber char(11) NOT NULL
+    PhoneNumber char(11) NOT NULL,
+    PRIMARY KEY(Cust_ID)
 );
 
 CREATE TABLE Airplane (
     Seat_Location_ID char(5) NOT NULL,
     Total_seat char(5) NOT NULL,
     Plane_ID char(9) NOT NULL,
-    Plane_model varchar(255) NOT NULL
+    Plane_model varchar(255) NOT NULL,
+    PRIMARY KEY(Plane_ID)
 );
 
 CREATE TABLE Airplane_Schedule(
@@ -23,7 +25,11 @@ CREATE TABLE Airplane_Schedule(
     Plane_ID char(11),
     Time_Depart char(9),
     Time_Land char(9),
-    Gate_Num char(9)
+    Gate_Num char(9),
+    PRIMARY KEY(Schedule_ID),
+    Foreign Key(Plane_ID) references Airplane(Plane_ID),
+    Foreign Key(Location_ID_start) references Airport(Plane_ID),
+    Foreign Key(Location_ID_end) references Airport(Plane_ID)
 );
 
 CREATE TABLE Ticket(
@@ -32,25 +38,32 @@ CREATE TABLE Ticket(
     Schedule_ID char(9),
     Seat_Location_ID char(9),
     Type_ID char(9),
+    PRIMARY KEY(Ticket_ID),
+    Foreign Key(Cust_ID) references Customer(Cust_ID),
+    Foreign Key(Schedule_ID) references Airplane_Schedule(Schedule_ID),
+    Foreign Key(Seat_Location_ID) references Airplane(Seat_Location_ID),
+    Foreign Key(Type_ID) references TicketType(Type_ID)
 );
 
 CREATE TABLE Airport (
     Airport_name varchar(255) NOT NULL,
-    Airport_ID char(9)  NOT NULL
+    Airport_ID char(9)  NOT NULL,
+    PRIMARY KEY(Airport_ID)
 );
 
 create table TicketType(
-Type_ID char(9) NOT NULL,
-Price Decimal(10,2),
-Mem_ID char(9),
-PRIMARY KEY(Type_ID)
+    Type_ID char(9) NOT NULL,
+    Price Decimal(10,2),
+    Mem_ID char(9),
+    PRIMARY KEY(Type_ID),
+    Foreign Key(Mem_ID) references Membership(Mem_ID)
 );
 
 create table Membership(
-Category varchar(9) Not Null,
-Mem_ID char(9) Not Null,
-Mem_Name varchar(9) NOT NULL,
-Mem_Start DATE NOT NULL,
-Mem_End DATE NOT NULL,
-PRIMARY KEY(Mem_ID)
+    Category varchar(9) Not Null,
+    Mem_ID char(9) Not Null,
+    Mem_Name varchar(9) NOT NULL,
+    Mem_Start DATE NOT NULL,
+    Mem_End DATE NOT NULL,
+    PRIMARY KEY(Mem_ID)
 );
