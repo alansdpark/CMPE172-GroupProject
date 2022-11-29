df84f508e4b5e7579d854c9761298c86bd96dad7
Alan Park <28246519+alansdpark@users.noreply.github.com>
2022-11-29 10:44:19 -0800

    edited

--- IntermediateQueries.sql
+++ IntermediateQueries.sql
@@ -5,4 +5,4 @@ select Plane_model, Total_seat, Location_ID_start, Location_ID_end, Date_Depart,
 select FirstName, LastName, Ticket_ID from Customer as a JOIN Ticket as b ON a.Cust_ID=b.Cust_ID;
 
 -- Get customers at least silver or gold tier
-select FirstName, LastName from Customer as a join (select * from Membership where Category='Gold' OR Category='Silver') as b on a.Cust_ID=b.Mem_ID;
+select FirstName, LastName, Category from Customer as a join (select * from Membership where Category='Gold' OR Category='Silver') as b on a.Cust_ID=b.Mem_ID;
